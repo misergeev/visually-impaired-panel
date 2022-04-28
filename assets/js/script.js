55e8aa8ac61ce96fd6d4ebbf1e88b77e7dbff669
Michail <mialsergeev@gmail.com>
2022-04-29 00:23:45 +0500

    First structure

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+if (!window.jQuery) {
+    document.write('<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>')
+    // console.log('podkl');
+} else {
+    // console.log('nepodkl');
+}
