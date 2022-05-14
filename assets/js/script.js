d3e797b0beccb7e6b8fadd4e9ff5d5b4d7bf8352
Michail <mialsergeev@gmail.com>
2022-05-14 18:58:30 +0500

    More css and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,34 @@ if (!window.jQuery) {
 }
 
 function plugin() {
+    function color() {
+        $('body').removeClass('whiteblack');
+        $('body').removeClass('blackwhite');
+        $('body').removeClass('blue');
+        $('body').removeClass('brown');
+    }
+
+    function size() {
+        $('body').removeClass('size18');
+        $('body').removeClass('size22');
+        $('body').removeClass('size26');
+    }
+
     $(document).ready(function() {
-        
+        $(document).on('click', '.js-color-button', function() {
+            color();
+            var dataColor = $(this).attr('data-color');
+            $('body').addClass(dataColor);
+        })
     });
+
+    $(document).ready(function() {
+        $(document).on('click', '.js-size-button', function() {
+            size();
+            var dataColor = $(this).attr('data-size');
+            $('body').addClass(dataColor);
+        })
+    });
+
+
 }
